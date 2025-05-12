935ceaebc1e72c56756b1bdda0385f6742b98b17
giovannirottoli <giovanni.rottoli@artekium.com>
2025-05-12 11:42:23 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -55,12 +55,12 @@ def search(cuil):
     else:
         url = f"https://api.bcra.gob.ar/CentralDeDeudores/v1.0/Deudas/{cuil}"
         response = rq.get(url, verify=False)
-        
         if response.status_code == 500:
-            st.error(f'Error al obtener info desde el BCRA por un problema interno \n ' + pprint.pformat(response.content), icon="🚨")
-        if response.status_code == 400:
+            st.error(f'Error por un problema en la API del BCRA. Debería solucionarse pronto. \n. Status {response.status_code}', icon="🚨")
+            st.error(f'Utilizar la central de deudores momentáneamente: https://www.bcra.gob.ar/BCRAyVos/Situacion_Crediticia.asp')
+        elif response.status_code == 400:
             st.error(f"Parámetro erróneo: Ingresar 11 dígitos para realizar la consulta.", icon="🚨")
-        if response.status_code == 404:
+        elif response.status_code == 404:
             st.info(f"No se encuentran deudas para el CUIL especificado: {cuil}", icon="🕵️‍♂️" )
         else:
             j = response.json()
